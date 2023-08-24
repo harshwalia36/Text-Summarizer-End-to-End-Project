d95f65e69ef4751b345655f800f80bc4271c03e0
harshwalia36 <harsh.p.walia@gmail.com>
2023-08-24 17:01:19 +0530

    Data Ingestion completed

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,7 @@
+# save the path of config file and params file as they are constants
+
+from pathlib import Path
+
+CONFIG_FILE_PATH= Path("config/config.yaml")
+PARAMS_FILE_PATH= Path("params.yaml")
+
